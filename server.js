53d49fd8b4478980e08500e0e507ebc9f296dd4e
andrew <ap@ampiusa.com>
2021-04-23 12:05:52 -0700

    bug correction

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-
-
-const PORT = process.env.PORT || 3989;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
 const app = express();
 
 app.use(logger("dev"));
@@ -14,20 +13,16 @@ app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI || 'mongo://localhost/Transaction', {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
-
-mongoose.set('useCreateIndex', true);
-mongoose.set('debug', true);
 
 // routes
 app.use(require("./routes/api.js"));
-
 
 app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT}`);
