959bfa192e640e24f056e57344ebf7391c2042c6
andrew <ap@ampiusa.com>
2021-04-23 15:36:37 -0700

    k

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,45 +47,44 @@ self.addEventListener('activate', function(e) {
 //     self.clients.claim()
 });
 
-// Intercept Fetch requests
-self.addEventListener('fetch', function(e) {
-    if (e.request.url.includes('/api/')) {      
-      e.respondWith(
-        caches
-          .open(DATA_CACHE_NAME)
-          .then(cache => {
-            return fetch(e.request)
-              .then(response => {
-                console.log(response);
-                // If the response was good, clone it and store it in the cache.
-                if (response.status === 200) {
-                  cache.put(e.request.url, response.clone());
-                }
-  
-                return response;
-              })
-              .catch(error => {
-                console.log(error);
-                // Network request failed, try to get it from the cache.
-                return cache.match(e.request);
-              });
-          })
-          .catch(error => console.log(error))
-      );
-  
-      return;
-    }
-  
-    e.respondWith(
-      fetch(e.request).catch(function() {
-        return caches.match(e.request).then(function(response) {
-          if (response) {
-            return response;
-          } else if (e.request.headers.get('accept').includes('text/html')) {
-            // return the cached home page for all requests for html pages
-            return caches.match('/');
-          }
-        });
-      })
+
+// Intercept fetch requests
+self.addEventListener('fetch', function(evt) {
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch(err => console.log(err))
     );
-  });
+
+    return;
+  }
+
+  evt.respondWith(
+    fetch(evt.request).catch(function() {
+      return caches.match(evt.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get('accept').includes('text/html')) {
+          // return the cached home page for all requests for html pages
+          return caches.match('/');
+        }
+      });
+    })
+  );
+});
