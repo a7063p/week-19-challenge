29fe3591f9becf41e7550c76743f809db09389b4
andrew <ap@ampiusa.com>
2021-04-23 11:12:48 -0700

    update

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,9 +16,9 @@ request.onsuccess = function(e) {
     // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
     db = e.target.result;  
     // check if app is online, if yes run checkDatabase() function to send all local db data to api
-    // if (navigator.onLine) {
+    if (navigator.onLine) {
        uploadTransaction();
-    // }
+    }
   };  
   request.onerror = function(e) {
     // log error here
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 
+
 const PORT = process.env.PORT || 3989;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 const app = express();
@@ -13,7 +14,6 @@ app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI || 'mongo://localhost/Transaction', {
@@ -27,6 +27,7 @@ mongoose.set('debug', true);
 
 // routes
 app.use(require("./routes/api.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT}`);
