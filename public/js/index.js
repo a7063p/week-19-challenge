1a7b5999f6d2208b9f38ea82101714fbf62225aa
andrew <ap@ampiusa.com>
2021-04-23 18:41:16 -0700

    test

--- public/js/index.js
+++ public/js/index.js
@@ -41,43 +41,43 @@ let myChart;
 //   });
 // }
 
-// function populateChart() {
-//   // copy array and reverse it
-//   let reversed = transactions.slice().reverse();
-//   let sum = 0;
+function populateChart() {
+  // copy array and reverse it
+  let reversed = transactions.slice().reverse();
+  let sum = 0;
 
-//   // create date labels for chart
-//   let labels = reversed.map(t => {
-//     let date = new Date(t.date);
-//     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-//   });
+  // create date labels for chart
+  let labels = reversed.map(t => {
+    let date = new Date(t.date);
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  });
 
-//   // create incremental values for chart
-//   let data = reversed.map(t => {
-//     sum += parseInt(t.value);
-//     return sum;
-//   });
+  // create incremental values for chart
+  let data = reversed.map(t => {
+    sum += parseInt(t.value);
+    return sum;
+  });
 
-//   // remove old chart if it exists
-//   if (myChart) {
-//     myChart.destroy();
-//   }
+  // remove old chart if it exists
+  if (myChart) {
+    myChart.destroy();
+  }
 
-//   let ctx = document.getElementById("myChart").getContext("2d");
+  let ctx = document.getElementById("myChart").getContext("2d");
 
-//   myChart = new Chart(ctx, {
-//     type: 'line',
-//       data: {
-//         labels,
-//         datasets: [{
-//             label: "Total Over Time",
-//             fill: true,
-//             backgroundColor: "#6666ff",
-//             data
-//         }]
-//     }
-//   });
-// }
+  myChart = new Chart(ctx, {
+    type: 'line',
+      data: {
+        labels,
+        datasets: [{
+            label: "Total Over Time",
+            fill: true,
+            backgroundColor: "#6666ff",
+            data
+        }]
+    }
+  });
+}
 
 function sendTransaction(isAdding) {
   let nameEl = document.querySelector("#t-name");
